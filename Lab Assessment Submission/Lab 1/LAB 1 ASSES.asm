c73023041a158a7eca02f4cf2d16719787a15421
taslimatamannansu <taslima.tamanna@northsouth.edu>
2020-11-11 11:11:30 +0600

    Files Added

---
+++ Lab Assessment Submission/Lab 1/LAB 1 ASSES.asm
@@ -0,0 +1,20 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h
+
+  ;#include<stdio.h> 
+  MOV AX, 1   
+ADD al,5 
+ADD AL,5  
+; add your code here
+
+ret; 
+
+
+
+
+
+
+                
